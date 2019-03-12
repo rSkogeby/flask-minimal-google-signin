6770e37f93b3bafc7f4539979904bf2391663387
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-12 09:50:16 +0000

    Add redirect_uris from instance/client_secrets

--- google_auth.py
+++ google_auth.py
@@ -9,6 +9,10 @@ from flask import Flask, jsonify, request
 from flask import session as login_session
 from oauth2client.client import flow_from_clientsecrets
 from OpenSSL import SSL
+import json
+
+client_secrets_file = open('instance/client_secrets').read()
+client_secrets = json.loads(client_secrets_file).get('web')
 
 
 app = Flask(__name__)
@@ -19,7 +23,7 @@ def index():
     login_session['state'] = passthrough_value
     flow = flow_from_clientsecrets('instance/client_secrets',
                                 scope='',
-                                redirect_uri='postmessage')
+                                redirect_uri=client_secrets.get('redirect_uris')[0])
                                 #access_type='offline', # Return both access and refresh token
                                 #state=passthrough_value, # Provide a client Nonce
                                 #include_granted_scopes='true') # Allow incremental authorization
@@ -34,7 +38,9 @@ def index():
     #credentials = flow.step2_exchange(code)
     return "Index, auth_uri: {}, state: {}, code: {}".format(auth_uri, login_session['state'], code.decode())
 
-
+@app.route('/login/authorized')
+def callback():
+    
 
 def main():
     app.secret_key = 'a_very_secret_key'
