0d618fdc43e146f4a10ae9a2d339946f42c62fec
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-12 16:05:36 +0000

    Working minimal login

--- google_auth.py
+++ google_auth.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" """
+"""Minimal Google login using OAuth 2.0 and the library oauth2client.
+
+oauth2client has been deprecated in favor for google-auth. This
+code, however, still works fine as of 12-03-2019."""
 
 import os
 import string
@@ -7,9 +10,11 @@ import random
 import httplib2
 import requests
 
-from flask import Flask, jsonify, request, render_template, url_for, redirect
+from flask import Flask, jsonify, request, render_template, url_for, redirect, make_response
 from flask import session as login_session
 from oauth2client.client import flow_from_clientsecrets
+
+from google.oauth2 import credentials
 from OpenSSL import SSL
 import json
 
@@ -36,9 +41,6 @@ def gconnect():
     flow = flow_from_clientsecrets('instance/client_secrets',
                                 scope='https://www.googleapis.com/auth/userinfo.email',
                                 redirect_uri=client_secrets.get('redirect_uris')[0])
-                                #access_type='offline', # Return both access and refresh token
-                                #state=passthrough_value, # Provide a client Nonce
-                                #include_granted_scopes='true') # Allow incremental authorization
     # Redirect the user to auth_uri on your platform.
     auth_uri = flow.step1_get_authorize_url()
     #login_session['flow'] = flow
@@ -48,25 +50,27 @@ def gconnect():
 @app.route('/login/authorized')
 def callback():
     code = request.args.get('code')
-    session_state = request.args.get('session_state')
-    scope = request.args.get('scope')
-    authuser = request.args.get('authuser')
-    prompt = request.args.get('prompt')
-
     # Pass code provided by authorization server redirection to this function
     flow = flow_from_clientsecrets('instance/client_secrets',
                                 scope='https://www.googleapis.com/auth/userinfo.email',
-                                redirect_uri=client_secrets.get('redirect_uris')[0])
+                                redirect_uri=client_secrets.get('redirect_uris')[0]) 
     credentials = flow.step2_exchange(code)
-    # Fetch some info
+    # Supply access token to information request using httplib2
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}'.\
         format(access_token))
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
+    # Check user is not already logged in using gconnect
     gplus_id = credentials.id_token['sub']
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials != None and gplus_id == stored_gplus_id:
+        response = make_response(
+            json.dumps("Current user is already connected."), 200
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
     # Store the access token in session for later use
     login_session['credentials'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
@@ -80,15 +84,6 @@ def callback():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
     return 'Code: {}, credentials: {}, result: {}, email: {}'.format(code, credentials, result, login_session['username'])
-
-    # Listen for response from authorization server
-    #h = httplib2.Http('.cache')
-    #resp, content = h.request(auth_uri, 'GET')
-    #result = json.loads(resp)
-
-
-    #return "Index, auth_uri: {}, state: {}, code: {}".format(auth_uri, login_session['state'], code.decode())
-
     
 
 def main():
