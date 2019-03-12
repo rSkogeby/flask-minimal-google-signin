15efac7afa17ffc58fef400e580e6beff6695913
Richard Skogeby <richard.skogeby@outlook.com>
2019-03-12 10:29:55 +0000

    Make sign in work until callback

--- google_auth.py
+++ google_auth.py
@@ -4,8 +4,9 @@
 import os
 import string
 import random
+import httplib2
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, url_for, redirect
 from flask import session as login_session
 from oauth2client.client import flow_from_clientsecrets
 from OpenSSL import SSL
@@ -18,28 +19,44 @@ client_secrets = json.loads(client_secrets_file).get('web')
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+@app.route('/login/')
+def login():
+    template_args = {}
     passthrough_value = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
     login_session['state'] = passthrough_value
+    template_args['state'] = login_session['state']
+
+    return render_template('login.html', args=template_args)
+    
+@app.route('/login/gconnect', methods=['GET', 'POST'])
+def gconnect():
     flow = flow_from_clientsecrets('instance/client_secrets',
-                                scope='',
+                                scope='openid',
                                 redirect_uri=client_secrets.get('redirect_uris')[0])
                                 #access_type='offline', # Return both access and refresh token
                                 #state=passthrough_value, # Provide a client Nonce
                                 #include_granted_scopes='true') # Allow incremental authorization
-
     # Redirect the user to auth_uri on your platform.
     auth_uri = flow.step1_get_authorize_url()
+    return redirect(auth_uri)
 
-    # Listen for response from authorization server
-    code = request.data
-
-    # Pass code provided by authorization server redirection to this function
-    #credentials = flow.step2_exchange(code)
-    return "Index, auth_uri: {}, state: {}, code: {}".format(auth_uri, login_session['state'], code.decode())
 
 @app.route('/login/authorized')
 def callback():
+    return "Callback"
+
+    # Pass code provided by authorization server redirection to this function
+    #credentials = flow.step2_exchange(code)
+
+    # Listen for response from authorization server
+    #h = httplib2.Http('.cache')
+    #resp, content = h.request(auth_uri, 'GET')
+    #result = json.loads(resp)
+    #code = request.data
+
+
+    #return "Index, auth_uri: {}, state: {}, code: {}".format(auth_uri, login_session['state'], code.decode())
+
     
 
 def main():
